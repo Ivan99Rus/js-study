1964e427f6ff6269f2e40f2f4b5692d60dedda20
Ivan99Rus <ivananapa@mail.ru>
2020-06-12 13:37:20 +0300

    feat: add lesson 03 simple and hard

---
+++ 03/hard/js/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let lang = prompt('Введите язык: ru, либо en');
+
+if (lang === 'ru') {
+  console.log('Понедельник\nВторник\nСреда\nЧетверг\nПятница\nСуббота\nВоскресенье');
+} else if (lang === 'en') {
+  console.log('monday\ntuesday\nwednesday\nthursday\nfriday\nsaturday\nsunday');
+} else {
+  console.log('Упс! Что-то пошло не так :(');
+}
+
+switch (lang) {
+  case 'ru':
+    console.log('Понедельник\nВторник\nСреда\nЧетверг\nПятница\nСуббота\nВоскресенье');
+    break;
+  case 'en':
+    console.log('monday\ntuesday\nwednesday\nthursday\nfriday\nsaturday\nsunday');
+    break;
+  default:
+    console.log('Упс! Что-то пошло не так :(');
+    break;
+}
+
+let answers = [{
+    arrLang: 'ru', days: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+  },
+  {
+    arrLang: 'en', days: ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+  }
+];
+
+let answersResArr = answers.filter(item => item.arrLang === lang);
+let resTotal  = answersResArr[0].days;
+
+console.log(resTotal.join('\n'));
---
+++ 03/simple/js/main.js
@@ -0,0 +1,43 @@
+let money = 1000000000,
+  income = 'Инвестиции',
+  addExpenses = "Обучение, Коммунальные услуги, Интернет",
+  deposit = true,
+  mission = 1000000000,
+  period = 9;
+
+console.log("money: " + typeof money);
+console.log("income: " + typeof income);
+console.log("deposit: " + typeof deposit);
+console.log("length of addExpenses: " + addExpenses.length);
+console.log("Период равен: " + period + " месяцев");
+console.log("Цель заработать: " + mission + " долларов");
+console.log(addExpenses.toLowerCase().split(', '));
+
+
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = Boolean(prompt('Есть ли у вас депозит в банке?'));
+
+let expenses1 = prompt('Введите обязательную статью расходов?'),
+  amount1 = +prompt('Во сколько это обойдется?'),
+  expenses2 = prompt('Введите обязательную статью расходов?'),
+  amount2 = +prompt('Во сколько это обойдется?'),
+  budgetMonth = amount1 + amount2;
+  console.log('Бюджет на месяц: ' + budgetMonth);
+  console.log('Цель будет достигнута за ' + Math.ceil( mission / budgetMonth ) + ' месяцев');
+
+let budgetDay = budgetMonth / 30;
+console.log('Бюджет на день: ' + Math.floor(budgetDay));
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+}
+if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+}
+if (budgetDay < 600) {
+  console.log('К сожалению, у вас уровень дохода ниже среднего');
+}
+if (budgetDay <= 0) {
+  console.log('Что-то пошло не так');
+}
