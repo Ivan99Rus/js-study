aed23db1182074e57784d5a337009353375a4696
Ivan99Rus <ivananapa@mail.ru>
2020-06-17 01:15:11 +0300

    feat: add lesson 09 simple

---
+++ 09/simple/js/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const btnCalculate = document.getElementById('start'),
+  incomeAdd = document.querySelector('.income_add'),
+  expensesAdd = document.querySelector('.expenses_add'),
+  depositCheck = document.querySelector('#deposit-check'),
+  additionalIncomeItems = document.querySelectorAll('.additional_income-item'),
+  budgetDayValue = document.querySelector('.budget_day-value'),
+  expensesMonthValue = document.querySelector('.expenses_month-value'),
+  additionalIncomeValue = document.querySelector('.additional_income-value'),
+  additionalExpensesValue = document.querySelector('.additional_expenses-value'),
+  incomePeriodValue = document.querySelector('.aincome_period-value'),
+  targetMonthValue = document.querySelector('.target_month-value'),
+  salaryAmount = document.querySelector('.salary-amount'),
+  incomeTitle = document.querySelector('.income-title'),
+  incomeAmount = document.querySelector('.income-amount'),
+  expensesTitle = document.querySelector('.expenses-title'),
+  expensesAmount = document.querySelector('.expenses-amount'),
+  additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+  targetAmount = document.querySelector('.target-amount'),
+  range = document.querySelector('.range');
+  
+  
+  
+  
+  
+  
