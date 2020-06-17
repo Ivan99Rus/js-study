839fdd9aed550ac3af760e5fafa8186db66b1376
Ivan99Rus <ivananapa@mail.ru>
2020-06-17 18:04:50 +0300

    refact: bug fixed

--- 09/simple/js/index.js
+++ 09/simple/js/index.js
@@ -1,27 +1,27 @@
 'use strict';
 
 const btnCalculate = document.getElementById('start'),
-  incomeAdd = document.querySelector('.income_add'),
-  expensesAdd = document.querySelector('.expenses_add'),
+  incomeAdd = document.getElementsByTagName('button')[0],
+  expensesAdd = document.getElementsByTagName('button')[1],
   depositCheck = document.querySelector('#deposit-check'),
   additionalIncomeItems = document.querySelectorAll('.additional_income-item'),
   budgetDayValue = document.querySelector('.budget_day-value'),
   expensesMonthValue = document.querySelector('.expenses_month-value'),
   additionalIncomeValue = document.querySelector('.additional_income-value'),
   additionalExpensesValue = document.querySelector('.additional_expenses-value'),
-  incomePeriodValue = document.querySelector('.aincome_period-value'),
+  incomePeriodValue = document.querySelector('.income_period-value'),
   targetMonthValue = document.querySelector('.target_month-value'),
   salaryAmount = document.querySelector('.salary-amount'),
-  incomeTitle = document.querySelector('.income-title'),
+  incomeTitle = document.querySelectorAll('.income-title')[1],
   incomeAmount = document.querySelector('.income-amount'),
-  expensesTitle = document.querySelector('.expenses-title'),
+  expensesTitle = document.querySelectorAll('.expenses-title')[1],
   expensesAmount = document.querySelector('.expenses-amount'),
   additionalExpensesItem = document.querySelector('.additional_expenses-item'),
   targetAmount = document.querySelector('.target-amount'),
   range = document.querySelector('.range');
   
-  
-  
+  console.log('expensesAdd: ', expensesAdd);
+  console.log('incomeAdd: ', incomeAdd);
   
   
   
