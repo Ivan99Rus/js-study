e4f7fc27237b5efc9e499647fda848af07c25409
Ivan99Rus <ivananapa@mail.ru>
2020-06-17 16:51:22 +0300

    feat: add lesson 07 hard

--- 07/hard/js/main.js
+++ 07/hard/js/main.js
@@ -1,1 +1,14 @@
-"use strict";
+'use strict';
+
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  day = Number(new Date().getDay() - 1);
+
+week.forEach((item, index, arr) => {
+  if (index === day) {
+    document.write(`<p><b>${item}</b></p>`);
+  } else if (index === 5 || index === 6) {
+    document.write(`<p><i>${item}</i></p>`);
+  } else {
+    document.write(`<p>${item}</p>`);
+  }
+});
